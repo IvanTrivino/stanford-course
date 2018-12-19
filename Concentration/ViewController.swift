a87f5f654f6a2fe5c6a0dd11ed95293b3cf8b806
Ivan Camilo Triviño López <ivancamilo.trivi@icloud.com>
2018-12-19 18:51:11 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ivan Camilo Triviño López on 12/19/18.
+//  Copyright © 2018 Ivan Camilo Triviño López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
